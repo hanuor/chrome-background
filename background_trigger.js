0ab9d06299be8d27737e674c6d64ddb2e0a06ab4
hanuor <hanuortech@gmail.com>
2017-11-16 23:11:45 +0530

    Added some more images

--- background_trigger.js
+++ background_trigger.js
@@ -1,1 +1,5 @@
-document.body.style.backgroundImage = "url('https://wallpaperscraft.com/image/mountain_river_snow_nature_92721_1920x1080.jpg')";
+document.body.style.backgroundImage = "url('https://github.com/hanuor/chrome-background/blob/master/backgrounds/milky.jpg?raw=true')";
+//enter the minimum number
+var minimum = 0
+var maximum = 3
+var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
